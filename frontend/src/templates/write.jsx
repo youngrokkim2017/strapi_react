a09e326aa6bb6443515da2fe08a35edda90161bb
Patrick <patrick.yrkim@gmail.com>
2021-04-07 00:06:04 -0700

    Initialize single content types

---
+++ frontend/src/templates/write.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import Strapi from 'strapi-sdk-javascript/build/main'
+import ReactMarkdown from "react-markdown"
+
+const strapi = new Strapi('http://localhost:1337');
+
+class WriteForUsTemplate extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const data = await strapi.getEntries('write')
+            this.setState({ data });
+        } 
+        catch(err) {
+            alert(err);
+        }
+    }
+
+    render() {
+        let data = this.state.data;
+
+        return (
+            <div>
+                <h2 className="font-normal mb-8 text-4xl leading-tight">{data.title}</h2>
+                  <div className="flex-grow flex-shrink-0 prose tracking-normal text-black max-w-full">
+                    <ReactMarkdown source={data.content} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default WriteForUsTemplate;
